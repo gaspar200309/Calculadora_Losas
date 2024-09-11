d8d6fbc32ab0dc5f2e82c33a60e5776596ab6644
Adam Jhoel <amercer732@gmail.com>
2024-09-11 09:51:15 -0400

    Feat: refactoring labels and titles

--- Inteface/Principal.py
+++ Inteface/Principal.py
@@ -4,66 +4,52 @@ import tkinter.ttk as ttk
 from tkinter.constants import *
 import os.path
 
-_location = os.path.dirname(__file__)
+colorDef = '#d9d9d9'
+colorGrey = '#F0F0F0'
+title_config = {
+    "background": colorDef,
+    "font": ("Comic Sans MS", 15, "bold"),
+    "foreground": "black",
+    "anchor": "w",
+}
 
-
-_bgcolor = '#d9d9d9'
-_fgcolor = '#000000'
-_tabfg1 = 'black' 
-_tabfg2 = 'white' 
-_bgmode = 'light' 
-_tabbg1 = '#d9d9d9' 
-_tabbg2 = 'gray40' 
-
-_style_code_ran = 0
-
+label_config = {
+    "background": colorDef,
+    "font": ("Comic Sans MS", 11),
+    "foreground": "black",
+    "anchor": "w",   
+}
 
 class principal:
     def __init__(self, top=None):
-        '''This class configures and populates the toplevel window.
-           top is the toplevel containing window.'''
-
         top.geometry("787x624+548+193")
         top.minsize(120, 1)
         top.maxsize(1924, 1061)
         top.resizable(1,  1)
         top.title("Toplevel 0")
         top.configure(background="#d9d9d9")
-        top.configure(highlightbackground="#d9d9d9")
-        top.configure(highlightcolor="#000000")
-
         self.top = top
 
         self.Button1 = tk.Button(self.top)
         self.Button1.place(relx=0.496, rely=0.929, height=26, width=47)
         self.Button1.configure(background="#fefda6")
         self.Button1.configure(font="-family {Segoe UI} -size 9")
-        self.Button1.configure(text='''EXIT''')
+        self.Button1.configure(text='''EXIT''', command=self.top.destroy)
 
         self.TNotebook1 = ttk.Notebook(self.top)
-        self.TNotebook1.place(relx=0.017, rely=0.022, relheight=0.894
-                , relwidth=0.972)
+        self.TNotebook1.place(relx=0.017, rely=0.022, relheight=0.894, relwidth=0.972)
         self.theEc1 = tk.Frame(self.TNotebook1)
         self.TNotebook1.add(self.theEc1, padding=3)
-        self.TNotebook1.tab(0, text='''Ecuacion 1''', compound="left"
-                ,underline='''-1''', )
+        self.TNotebook1.tab(0, text='''Ecuacion 1''', compound="left",underline='''-1''', )
         self.theEc1.configure(background="#d9d9d9")
-        self.theEc1.configure(highlightbackground="#d9d9d9")
-        self.theEc1.configure(highlightcolor="#000000")
         self.theEc2 = tk.Frame(self.TNotebook1)
         self.TNotebook1.add(self.theEc2, padding=3)
-        self.TNotebook1.tab(1, text='''Ecuacion 2''', compound="left"
-                ,underline='''-1''', )
+        self.TNotebook1.tab(1, text='''Ecuacion 2''', compound="left",underline='''-1''', )
         self.theEc2.configure(background="#d9d9d9")
-        self.theEc2.configure(highlightbackground="#d9d9d9")
-        self.theEc2.configure(highlightcolor="#000000")
         self.theEc3 = tk.Frame(self.TNotebook1)
         self.TNotebook1.add(self.theEc3, padding=3)
-        self.TNotebook1.tab(2, text='''Ecuacion 3''', compound="left"
-                ,underline='''-1''', )
+        self.TNotebook1.tab(2, text='''Ecuacion 3''', compound="left",underline='''-1''', )
         self.theEc3.configure(background="#d9d9d9")
-        self.theEc3.configure(highlightbackground="#d9d9d9")
-        self.theEc3.configure(highlightcolor="#000000")
 
         self.btnCalcular = tk.Button(self.theEc1)
         self.btnCalcular.place(relx=0.472, rely=0.449, height=36, width=67)
@@ -84,25 +70,15 @@ class principal:
 
         self.Label1 = tk.Label(self.theEc1)
         self.Label1.place(relx=0.039, rely=0.131, height=21, width=34)
-        self.Label1.configure(anchor='w')
-        self.Label1.configure(background="#d9d9d9")
-        self.Label1.configure(compound='left')
-        self.Label1.configure(font="-family {Segoe UI} -size 9")
-        self.Label1.configure(text='''H''')
+        self.Label1.configure(**label_config,text='''H''')
 
         self.labD = tk.Label(self.theEc1)
         self.labD.place(relx=0.629, rely=0.225, height=21, width=34)
-        self.labD.configure(anchor='w')
-        self.labD.configure(background="#d9d9d9")
-        self.labD.configure(compound='left')
-        self.labD.configure(text='''d''')
+        self.labD.configure(**label_config,text='''d''')
 
         self.labRec = tk.Label(self.theEc1)
         self.labRec.place(relx=0.039, rely=0.318, height=21, width=34)
-        self.labRec.configure(anchor='w')
-        self.labRec.configure(background="#d9d9d9")
-        self.labRec.configure(compound='left')
-        self.labRec.configure(text='''Rec''')
+        self.labRec.configure(**label_config,text='''Rec''')
 
         self.entryH = tk.Entry(self.theEc1)
         self.entryH.place(relx=0.197, rely=0.137, height=20, relwidth=0.189)
@@ -117,20 +93,12 @@ class principal:
         self.entryAs.configure(insertbackground="#000000")
 
         self.labAso = tk.Label(self.theEc1)
-        self.labAso.place(relx=0.039, rely=0.225, height=21, width=74)
-        self.labAso.configure(anchor='w')
-        self.labAso.configure(compound='left')
-        self.labAso.configure(background="#d9d9d9")
-        self.labAso.configure(font="-family {Segoe UI} -size 9")
-        self.labAso.configure(text='''AsoPrincipal''')
+        self.labAso.place(relx=0.039, rely=0.225, height=21, width=94)
+        self.labAso.configure(**label_config,text='''AsoPrincipal''')
 
         self.labTitle = tk.Label(self.theEc1)
         self.labTitle.place(relx=0.38, rely=0.037, height=41, width=234)
-        self.labTitle.configure(anchor='w')
-        self.labTitle.configure(background="#d9d9d9")
-        self.labTitle.configure(compound='left')
-        self.labTitle.configure(font="-family {Segoe UI} -size 16")
-        self.labTitle.configure(text='''Titulo de la ecuacion 1''')
+        self.labTitle.configure(**title_config,text='''Titulo de la ecuacion 1''')
 
         self.Entry2 = tk.Entry(self.theEc2)
         self.Entry2.place(relx=0.642, rely=0.187, height=20, relwidth=0.189)
@@ -146,16 +114,11 @@ class principal:
 
         self.labMu = tk.Label(self.theEc2)
         self.labMu.place(relx=0.052, rely=0.206, height=21, width=34)
-        self.labMu.configure(anchor='w')
-        self.labMu.configure(background="#d9d9d9")
-        self.labMu.configure(compound='left')
-        self.labMu.configure(text='''Mu''')
+        self.labMu.configure(**label_config,text='''Mu''')
 
         self.labd = tk.Label(self.theEc2)
         self.labd.place(relx=0.577, rely=0.187, height=21, width=35)
-        self.labd.configure(anchor='w')
-        self.labd.configure(background="#d9d9d9")
-        self.labd.configure(text='''d''')
+        self.labd.configure(**label_config,text='''d''')
 
         self.btnCal2 = tk.Button(self.theEc2)
         self.btnCal2.place(relx=0.472, rely=0.375, height=36, width=67)
@@ -164,20 +127,11 @@ class principal:
 
         self.labTitle2 = tk.Label(self.theEc2)
         self.labTitle2.place(relx=0.367, rely=0.037, height=31, width=224)
-        self.labTitle2.configure(anchor='w')
-        self.labTitle2.configure(background="#d9d9d9")
-        self.labTitle2.configure(compound='left')
-        self.labTitle2.configure(font="-family {Segoe UI} -size 14")
-        self.labTitle2.configure(text='''Titulo de la ecuacion 2''')
+        self.labTitle2.configure(**title_config,text='''Titulo de la ecuacion 2''')
 
         self.labTitle3 = tk.Label(self.theEc3)
-        self.labTitle3.place(relx=0.38, rely=0.056, height=41, width=205)
-        self.labTitle3.configure(anchor='w')
-        self.labTitle3.configure(background="#d9d9d9")
-        self.labTitle3.configure(compound='left')
-        self.labTitle3.configure(font="-family {Segoe UI} -size 14")
-        self.labTitle3.configure(foreground="#000000")
-        self.labTitle3.configure(text='''Seleccione la ecuacion''')
+        self.labTitle3.place(relx=0.38, rely=0.056, height=41, width=255)
+        self.labTitle3.configure(**title_config,text='''Seleccione la ecuacion''')
 
         self.optn1 = tk.Button(self.theEc3)
         self.optn1.place(relx=0.354, rely=0.206, height=46, width=237)
@@ -197,14 +151,13 @@ class principal:
 
 
 def main(*args):
-    '''Main entry point for the application.'''
     global root
     root = tk.Tk()
     root.protocol('WM_DELETE_WINDOW', root.destroy)
-    # Crea un widget toplevel.
+    # Creamos un widget global
     global _top1, _w1
     _top1 = root
-    _w1 = principal(_top1)  # Llama a la función principal para configurar la ventana
+    _w1 = principal(_top1)# Llamamos a la funcion pa configurar la ventana
     root.mainloop()
 
 if __name__ == '__main__':
